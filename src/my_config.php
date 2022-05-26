074c04dd23008409c64e1dde3737022e29a52a2b
GinoPSC <92866955+GinoPSC@users.noreply.github.com>
2022-05-25 21:55:18 -0400

    test

--- src/my_config.php
+++ src/my_config.php
@@ -2,7 +2,7 @@
 	//Base de datos
 	define('DB_SERVER', 'localhost');
 	define('DB_USERNAME', 'root');
-	define('DB_PASSWORD', '');
+	define('DB_PASSWORD', 'ASDASDSAD');
 	define('DB_NAME', 'inv_schema');
 
 	$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);

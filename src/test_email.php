90361d8d1808f1546e157790e5183c0740b4cd46
GinoPSC <92866955+GinoPSC@users.noreply.github.com>
2022-05-26 00:31:20 -0400

    archivo de otro proyecto

--- src/test_email.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$to = "mafagolo.87@gmail.com";
-	$subject = "Pelotudo";
-	$message = "Mentira, sos un crak";
-	$headers = "From: rayman_raiman@hotmail.com";
-	 
-	mail($to, $subject, $message, $headers);
-?>
